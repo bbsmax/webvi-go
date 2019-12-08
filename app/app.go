9b54becb6d633c97aa5bf015e088b196eeca4230
nataekyun <bbsmax@nate.com>
2019-12-09 08:12:51 +0900

    golang-mvc

--- app/app.go
+++ app/app.go
@@ -1,10 +1,16 @@
 package app
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 	"log"
 	"net/http"
 	"webvi-go/controllers/users"
+
+	"github.com/pelletier/go-toml"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 const (
@@ -16,6 +22,29 @@ const (
 )
 
 func StartApp() {
+	config, err := toml.LoadFile("webvi-go/config.toml")
+	if err != nil {
+		fmt.Println("Error ", err.Error())
+	}
+
+	fmt.Println("config : ", config.Get("database.username").(string))
+
+	serverStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true&serverTimezone=Asia/Seoul&loc=local",
+		config.Get("database.username").(string),
+		config.Get("database.password").(string),
+		config.Get("database.server").(string),
+		config.Get("database.port").(string),
+		config.Get("database.schema").(string))
+
+	db, err := gorm.Open("mysql", serverStr)
+
+	if err != nil {
+		log.Panicf("error : [%v]", err)
+		panic(err)
+	}
+
+	defer db.Close()
+
 	router := mux.NewRouter()
 	router.HandleFunc("/user", users.Signup).Methods(http.MethodPost)
 
