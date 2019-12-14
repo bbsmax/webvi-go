9810760aece069a73bad17c609b9b386b9054a85
nataekyun <bbsmax@nate.com>
2019-12-15 08:27:33 +0900

    회원가입 validate 상세추가

--- front/dto/user.go
+++ front/dto/user.go
@@ -5,6 +5,10 @@ import (
 	"webvi-go/front/utils"
 )
 
+const (
+	passwordPattern = "/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,16}$/" //영문 + 특수문자 + 숫자정규식
+)
+
 type UserRequest struct {
 	ID       string `json:"ID"`
 	Name     string `json:"name"`
@@ -28,10 +32,10 @@ func (r UserRequest) Validate() error {
 	var errs error
 	if !utils.EmailCheck(r.Email) {
 		errs = errors.New("The email field should be a valid email address!")
-	} else if r.Password == "" {
-		errs = errors.New("The password field should be a required!")
-	} else if r.Name == "" {
-		errs = errors.New("The name field should be a required!")
+	} else if !utils.PasswordCheck(r.Password) {
+		errs = errors.New("Password must be 8 characters or more, special characters, numbers, and letters.")
+	} else if !utils.NameCheck(r.Name) {
+		errs = errors.New("PThe name must be at least 4 characters long.")
 	}
 	return errs
 }
